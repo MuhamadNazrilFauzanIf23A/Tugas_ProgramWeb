894ac71b75e11ad6217a71c6a677cc2ac06994fd
Muhamad Nazril Fauzan <if23.muhamadfauzan@mhs.ubpkarawang.ac.id>
2024-11-28 19:18:07 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>halaman</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>ini halaman login</h1>
-    <form action="./backend/login.php" method="POST">
-        <input type="email" name="email" id="email" placehorder="Email"/>
-        <input type="password" name="password" id="password" placehorder="password"/>
-        <button type="submit">Login</button>
-        </body>
+    <h1>login</h1>
+    <form action="./backend/login.php" method="post">
+        <input type="email" name="email" placeholder="masukkan email anda">
+        <input type="password" name="password" placeholder="masukkan password anda">
+        <input type="submit" value="login" name="submit">
+    </form>
+
+</body>
 </html>
